284e60ccfacb53722c514fa8254059e5e19f5fb5
bryan valdovinos <bvaldovinos24@gmail.com>
2023-04-28 15:29:17 -0700

    pulled attributes from object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
 function App() {
-  const title = 'React';
-
+  const welcome = {
+    greeting: 'hello',
+    title: 'react'
+  }
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>{welcome.greeting}{welcome.title}</h1>
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' />
     </div>
