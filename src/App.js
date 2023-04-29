67999bc661151ee58147de5ba811a2ad9d43d77d
bryan valdovinos <bvaldovinos24@gmail.com>
2023-04-28 17:12:51 -0700

    captitalized object properties

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import * as React from 'react';
 
 function App() {
   const welcome = {
-    greeting: 'hello',
-    title: 'react'
+    greeting: 'Hello',
+    title: 'React'
   }
   return (
     <div>
-      <h1>{welcome.greeting}{welcome.title}</h1>
+      <h1>{welcome.greeting} {welcome.title}</h1>
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' />
     </div>
