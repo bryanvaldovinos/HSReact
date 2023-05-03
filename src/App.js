4f0e4bc6cb142a77e26d7345a745f2104586db6c
bryan valdovinos <bvaldovinos24@gmail.com>
2023-05-03 11:15:09 -0700

    passed props from stories to list component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
 
-const App = () => (
+const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ]
+
+  return (
     <div>
       <h1>My Hacker Stories</h1>
 
@@ -27,14 +30,16 @@ const App = () => (
 
       <hr />
 
-      <List />
+      <List list = {stories}/>
 
     </div>
   );
+}
 
-  const List = () => (
+
+  const List = (props) => (
       <ul>
-        {list.map((item) => (
+        {props.list.map((item) => (
             <li key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
