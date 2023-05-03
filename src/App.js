d39d5a1df707ea48eaa0be67998d94c9eb6542b4
bryan valdovinos <bvaldovinos24@gmail.com>
2023-05-02 18:53:22 -0700

    made functions for both Lists and Search components

--- src/App.js
+++ src/App.js
@@ -23,28 +23,44 @@ function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' />
+
+      <Search />
 
       <hr />
 
+      <List />
+
+    </div>
+  );
+
+}
+
+  function List() {
+    return (
       <ul>
-        {list.map(function(item) {
+        {list.map(function (item) {
           return (
-          <li key={item.objectID}>
+            <li key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
               </span>
               <span> {item.author}</span>
               <span> {item.num_comments}</span>
               <span> {item.points}</span>
-          </li>
+            </li>
           )
         })}
       </ul>
-    </div>
-  );
+    )
+  }
 
-}
+  function Search() {
+    return (
+      <div>
+        <label htmlFor='search'>Search: </label>
+        <input id='search' type='text' />
+      </div>
+    )
+  }
 
 export default App;
