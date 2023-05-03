18e6b94f63e35e857aa2207295a16e8d00b5fd90
bryan valdovinos <bvaldovinos24@gmail.com>
2023-05-03 09:51:55 -0700

    added event handler on search

--- src/App.js
+++ src/App.js
@@ -48,11 +48,19 @@ const App = () => (
       </ul>
     )
 
-  const Search = () => (
+  const Search = () => {
+
+    const handleChange = (event) => {
+      console.log(event.target.value)
+    }
+
+    return (
       <div>
         <label htmlFor='search'>Search: </label>
-        <input id='search' type='text' />
+        <input id='search' type='text' onChange={handleChange} />
       </div>
-    )
+    );
+
+  }
 
 export default App;
