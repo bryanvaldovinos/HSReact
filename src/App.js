685897aac449740d6b70606c70e1b33c3e689190
bryan valdovinos <bvaldovinos24@gmail.com>
2023-05-17 11:46:25 -0700

    covered rest and spread, returned to initial variation to pass item props

--- src/App.js
+++ src/App.js
@@ -56,21 +56,14 @@ const List = ({list}) => (
       </ul>
     )
 
-const Item = ({
-  item: {
-    title,
-    url,
-    author,
-    num_comments,
-    points
-  }}) => (
+const Item = ({item}) => (
     <li>
     <span>
-      <a href={url}>{title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span> {author}</span>
-    <span> {num_comments}</span>
-    <span> {points}</span>
+    <span> {item.author}</span>
+    <span> {item.num_comments}</span>
+    <span> {item.points}</span>
   </li>
 )
 
